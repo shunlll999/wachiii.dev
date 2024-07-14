cdab5fe09dc0048475a698013b971d480d705df9
wachiii <wachara@mohara.co>
2024-07-14 23:40:00 +0700

    [UPDATE][MAIN] Resonsive niew

--- src/app/components/about/details/index.tsx
+++ src/app/components/about/details/index.tsx
@@ -14,6 +14,91 @@ const Details = ({ displayTexts, myParagraph, show, onClose }: DetailType) => {
   return (
     <div className={[styles.cover, show ? styles.show : ''].join(' ')}>
       <div className={`${styles.information} ${styles['add-long']}`}>
+        <CloseButton onClick={onClose}>
+          <div className={styles['close-icon']} />
+        </CloseButton>
+        <div className={styles.informationDetail}>
+          <div className={styles['avatar-circle']} />
+          <h3 className={styles['text-title']}>Wachiii <span>Liv Rafael</span></h3>
+          <div className={styles['text-title_subtitle']}><span>Software</span> Engineer</div>
+          <ul className={styles['personal-detail']}>
+            <li>Wachara Nilsonti</li>
+            <li>Rankhumhang 30/1, 2186/209, BKK (TH)</li>
+            <li>(+66)0845531451</li>
+            <li>rafael5715@gmail.com</li>
+          </ul>
+          <div className={styles['information-scroll']}>wheel your mouse ➜ ➜</div>
+        </div>
+        <div className={styles['information-more-detail']}>
+          <div className={styles['container-text']}>
+            <h1>Wachiii <span>Liv Rafael</span></h1>
+            <h2>{`I`}<span id='scramble'>{displayTexts}</span></h2>
+          </div>
+          <div className={styles.paragraph} dangerouslySetInnerHTML={{ __html: myParagraph }} />
+          <section className={styles['section-container']}>
+            <div className={styles['section-title']}>Language Skills</div>
+            <ul className={styles['section-item-list']}>
+              <li>TH</li>
+              <li>EN</li>
+            </ul>
+          </section>
+          <section className={styles['section-container']}>
+            <div className={styles['section-title']}>Education Timeline</div>
+            <Timeline data={Object.entries(schoolData)} />
+          </section>
+          <section className={styles['section-container']}>
+            <div className={styles['section-title']}>Experiences</div>
+            <ul className={`${styles['hide']} ${styles['section-item-list']}`}>
+              <li>
+                <span>ReactJS</span><br />
+                <progress data-value="85" className={styles['progress-bar']} value="85" max="100" />
+              </li>
+              <li>
+                <span>React Native</span><br />
+                <progress data-value="77" className={styles['progress-bar']} value="77" max="100" />
+              </li>
+              <li>
+                <span>Flutter</span><br />
+                <progress data-value="32" className={styles['progress-bar']} value="32" max="100" />
+              </li>
+              <li>
+                <span>Swift</span><br />
+                <progress data-value="25" className={styles['progress-bar']} value="25" max="100" />
+              </li>
+              <li>
+                <span>Kotlin</span><br />
+                <progress data-value="25" className={styles['progress-bar']} value="25" max="100" />
+              </li>
+              <li>
+                <span>C# (Unity)</span><br />
+                <progress data-value="50" className={styles['progress-bar']} value="50" max="100" />
+              </li>
+              <li>
+                <span>NodeJS</span><br />
+                <progress data-value="60" className={styles['progress-bar']} value="60" max="100" />
+              </li>
+            </ul>
+          </section>
+          <section className={styles['section-container']}>
+            <div className={styles['section-title']}>Quality Services</div>
+            <ul className={styles['section-item-list']}>
+              <li>Website developemnt</li>
+              <li>Mobile developemnt</li>
+            </ul>
+          </section>
+          <section className={styles['section-container']}>
+            <div className={styles['section-title']}>Working Timeline</div>
+            <Timeline data={Object.entries(experinceData).reverse()} />
+          </section>
+          <section className={`${styles['section-container']}`}>
+            <div className={styles['section-title']}>Clients Feedback</div>
+            <ul>
+              <li><i className={styles.empty}>- not yet include -</i></li>
+            </ul>
+          </section>
+        </div>
+      </div>
+      {/* <div className={`${styles.information} ${styles['add-long']}`}>
         <CloseButton onClick={onClose}>X</CloseButton>
         <div className={styles.informationDetail}>
           <div className={styles['avatar-circle']} />
@@ -95,7 +180,7 @@ const Details = ({ displayTexts, myParagraph, show, onClose }: DetailType) => {
             </ul>
           </section>
         </div>
-      </div>
+      </div> */}
     </div>
   )
 }
--- src/app/components/about/index.tsx
+++ src/app/components/about/index.tsx
@@ -16,10 +16,12 @@ const About = () => {
     ' do Mobile developer',
     ` feel 🍊`,
     ` play 🎮`,
-    ` eat 🍰`
+    ` eat 🍰`,
+    ` love to hangout with friends 🎉`,
   ]
 
   const [display, setDisplay] =useState('')
+  const [currentStep, setStep] = useState(0)
   const [show, setPopup] = useState(false)
   const ref = useRef<HTMLDivElement>(null)
 
@@ -31,29 +33,32 @@ const About = () => {
     gsap.timeline({repeat:-1, repeatDelay:3})
     .to('#scramble', {duration: 0.7, opacity: 0, onComplete: () => {
       step = (step < displayTexts.length -1) ? step+1 : 0
-      setDisplay(displayTexts[step]);
+      setDisplay(displayTexts[step])
+      setStep(step)
     }})
     .to('#scramble', {duration: 0.5, opacity: 1})
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
 
-  const myParagraph =  `Hello, I'm <span>Wachara Nilsonti</span>, though my friends affectionately call me <span>Wachiii</span>.I work as a Software Engineer in the bustling metropolis of Bangkok, Thailand 🇹🇭  <p>On this personal website, you'll discover not only my professional experiences but also insights into my personal life.</p>`;
+  const myParagraph =  `Hello, I'm <span class="subTitle">Wachara Nilsonti</span>, though my friends affectionately call me <span>Wachiii</span>. I work as a Software Engineer in the bustling metropolis of Bangkok, Thailand 🇹🇭  <p>On this personal website, you'll discover not only my professional experiences but also insights into my personal life.</p>`;
   return (
     <div id="panel" className={styles['flex-layout']}>
       <div className={styles.container}>
-        <div>
+        <div className={styles.avatarContainer}>
           <div className={styles.avatar}/>
           <div className={styles['avatar-original']}/>
         </div>
-        <div>
-          <div className={styles['container-text']}>
-            <h1>Wachiii <span>Liv Rafael</span></h1>
-            <h2>{`I`}<span id='scramble'>{display}</span></h2>
+        <div className={styles.informationContainer}>
+          <div>
+            <div className={styles['container-text']}>
+              <h1 className={styles.title}>Wachiii <span className={styles.subTitle}>Liv Rafael</span></h1>
+              <h2 className={[styles.title, styles.small].join(' ')}>{currentStep === displayTexts.length -1 ? 'And I' : 'I'}<span className={styles.subTitle} id='scramble'>{display}</span></h2>
+            </div>
+            <div className={styles.paragraph} dangerouslySetInnerHTML={{ __html: myParagraph }} />
           </div>
-          <div className={styles.paragraph} dangerouslySetInnerHTML={{ __html: myParagraph }} />
           <div className={styles['container-text-footer']} onClick={findMoreInfomation}>
-            <Button>Fine me more</Button>
+            <Button><span className={styles['btn-text']}>Fine me more</span></Button>
           </div>
         </div>
       </div>
