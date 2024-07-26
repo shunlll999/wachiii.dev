51ef237bcd9c4baffa82ca49da22b69fe1bc7501
wachiii <wachara@mohara.co>
2024-07-26 19:07:55 +0700

    [UPDATED][MAIN] Update the Top3 content

--- src/app/components/portfolio/index.tsx
+++ src/app/components/portfolio/index.tsx
@@ -3,6 +3,7 @@ import RecentCard from '../ui/comps/cards/recent'
 import GalleryTabBar from '../ui/cagergory/bar'
 import { useState } from 'react'
 import TopContentView from '../ui/comps/topContentView';
+import { describe } from 'node:test';
 
 const defaultGallery = [
   { type: 'webs' },
@@ -29,6 +30,33 @@ const defaultGallery = [
   { type: 'games' },
 ];
 
+const top3 = [
+  {
+    type: 'top3',
+    name: 'PaperBoy',
+    describe: '>With supporting text below as a natural lead-in to additional content.',
+    photo_url: '/assets/images/papaerboy_a.png',
+    product_year: '2022',
+    product_info: 1,
+  },
+  {
+    type: 'top3',
+    name: 'Preload game',
+    describe: 'Preload the game',
+    photo_url: '/assets/images/projects/html5Game/2014/game/cover.png',
+    product_year: '2022',
+    product_info: 2,
+  },
+  {
+    type: 'top3',
+    name: 'Kids games',
+    describe: 'Game for kids',
+    photo_url: '/assets/images/projects/kidsGame/2012/cover.png',
+    product_year: '2012',
+    product_info: 3,
+  }
+]
+
 const Portfolio = (props: any) => {
   const [gallery, setGallery] = useState(defaultGallery)
 
@@ -46,16 +74,19 @@ const Portfolio = (props: any) => {
       <TopContentView />
       <h2 className={styles.headline}>Top 3 Recents</h2>
       <div className={styles['recent-card-group']}>
+        {/* <RecentCard type='top3' />
         <RecentCard type='top3' />
-        <RecentCard type='top3' />
-        <RecentCard type='top3' />
+        <RecentCard type='top3' /> */}
+        {top3.map((item, key) => (
+           <RecentCard  key={key} type='top3' data={item} />
+        ))}
       </div>
       <h3>Gallery</h3>
       <GalleryTabBar onSelectType={onSelectType} />
       <div className={styles['recent-card-group']}>
-        {gallery.map((item, key) => (
+        {/* {gallery.map((item, key) => (
            <RecentCard key={key} type={item.type} onSelected={onSelectContent}/>
-        ))}
+        ))} */}
       </div>
     </div>
   )
--- src/app/components/ui/comps/cards/recent/index.tsx
+++ src/app/components/ui/comps/cards/recent/index.tsx
@@ -1,12 +1,27 @@
 /* eslint-disable @next/next/no-img-element */
 import styles from './recent.module.css'
 
-const RecentCard = ({ type, onSelected }: { type: string, onSelected?: (type: string) => void }) => {
+export type CardDataType = {
+  type: string
+  name: string
+  describe: string
+  photo_url: string
+  product_year: string
+  product_info: number
+}
+
+type RecentCardProps = {
+  onSelected?: (type: string) => void
+  data: CardDataType
+  type: string
+}
+
+const RecentCard = ({ type, onSelected, data }: RecentCardProps) => {
   return (
     <div data-type={type} className={styles.card} onClick={() => onSelected && onSelected(type)}>
-      <img className={styles['card-body-image']} src='/assets/images/papaerboy_a.png' alt='wachiii' />
-      <h5 className={styles['card-title']}>PaperBoy the game</h5>
-      <p className={styles['card-description']}>With supporting text below as a natural lead-in to additional content.</p>
+      <img className={styles['card-body-image']} src={data.photo_url} alt={data.name} />
+      <h5 className={styles['card-title']}>{data.name}</h5>
+      <p className={styles['card-description']}>{data.describe}</p>
       <div className={styles.badges}>
         <div className={styles.badge}><i className={`${styles.icon} ${styles.web}`}/></div>
         <div className={styles.badge}><i className={`${styles.icon} ${styles.game}`}/></div>
