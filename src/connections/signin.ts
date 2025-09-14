ef27571f81f24bb8c1923155ef6cd4c9c9e867ab
wachiii <wachara@mohara.co>
2025-09-14 14:45:44 +0700

    fix(portfolio): fetch portfolio from fireStore

--- src/connections/signin.ts
+++ src/connections/signin.ts
@@ -12,18 +12,15 @@ export default async function signIn(email: string, password: string) {
   try {
     result = await signInWithEmailAndPassword(auth, email, password);
     const dataStore = result.user.toJSON()
-    console.log('store', dataStore);
     userData = await getDocumentByID(COLLECTIONS.USER_COLLECTION, result.user.uid);
     const store = {
       ...userData.result?.data(),
       cache: dataStore
     }
     localStorage.setItem(`!1::user-cache`, JSON.stringify(store));
-    console.log('%c SIGN IN SUCCESS!', 'background: #222; color: #bada55');
 
   } catch (e: unknown) {
     error = (e as Error);
-    console.log(`%c ERROR!: ${(error as any)?.code}`, 'background: #222; color: #f1374f');
   }
   return { result: userData?.result?.data(), error };
 }
